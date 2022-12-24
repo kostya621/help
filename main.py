071328c1e72430c80636a503e71ee0db3f19d264
kostya <sigalovkostya@gmail.com>
2022-12-24 15:22:31 +0300

    all

---
+++ main.py
@@ -0,0 +1,33 @@
+import sys
+
+from PyQt5.QtSql import QSqlDatabase, QSqlTableModel
+from PyQt5.QtWidgets import QWidget, QTableView, QApplication
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        db = QSqlDatabase.addDatabase('QSQLITE')
+        db.setDatabaseName('coffee.sqlite')
+        db.open()
+        view = QTableView(self)
+        model = QSqlTableModel(self, db)
+        model.setTable('coff')
+        model.select()
+
+        view.setModel(model)
+        view.move(10, 10)
+        view.resize(617, 315)
+
+        self.setGeometry(300, 100, 650, 450)
+        self.setWindowTitle('Пример работы с QtSql')
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
